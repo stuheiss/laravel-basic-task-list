8479bc129553f8f0d4e95c01201262fb93fabf36
Stu Heiss <sheiss@highlandsolutions.com>
2016-04-03 10:58:01 -0500

    add routes for get, post, delete

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,29 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * Show Task Dashboard
+ */
+
 Route::get('/', function () {
-    return view('welcome');
+    //
+});
+
+/**
+ * Add New Task
+ */
+
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * Delete Task
+ */
+
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
