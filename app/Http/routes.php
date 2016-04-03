50413363aee660bd938c081eb0a25cd42db6dcbd
Stu Heiss <sheiss@highlandsolutions.com>
2016-04-03 13:48:34 -0500

    Adding Tasks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,7 +19,11 @@ use Illuminate\Http\Request;
  */
 
 Route::get('/', function () {
-    return view('tasks');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
 
 /**
@@ -27,7 +31,22 @@ Route::get('/', function () {
  */
 
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // Create The Task...
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 
 /**
